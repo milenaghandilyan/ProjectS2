94ae84aab703d59083ffffdf4cda5b77a9764863
Milena Ghandilyan <122803629+milenaghandilyan@users.noreply.github.com>
2026-04-28 22:43:11 +0400

    Update App.xaml.cs
    Removed XML summary comments from App.xaml.cs.

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,9 +8,7 @@ using System.Windows;
 
 namespace StudyPlannerUI2
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
+    
     public partial class App : Application
     {
     }
