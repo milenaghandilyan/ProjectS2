681a9c7732e25c6dea2f4168e08b869d72ec834b
Milena Ghandilyan <122803629+milenaghandilyan@users.noreply.github.com>
2026-05-06 22:30:52 +0400

    Add XML documentation for User class

--- User.cs
+++ User.cs
@@ -1,5 +1,8 @@
 ﻿namespace StudyPlannerUI2
 {
+     /// <summary>
+    /// Represents a user entity containing authentication credentials.
+    /// </summary>
     public class User
     {
         public string Username { get; set; }
